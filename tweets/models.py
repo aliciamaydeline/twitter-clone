4686bb7a4a57e324ab9f339e44d8d3f5f40a39b2
alicia.maydeline <amangto@uw.washington.edu>
2020-11-09 06:17:18 -0800

    adding user

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,17 @@
 import random
+from django.conf import settings
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
+
 class Tweet(models.Model):
+  # foreign key: many users can have many tweets, but one tweet can only have one user
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   content = models.TextField(blank=True, null=True)
   image = models.FileField(upload_to="images/", blank=True, null=True)
+
+  # def __str__(self):
+  #   return self.content
 
   class Meta:
     ordering = ['-id']  # descending order
