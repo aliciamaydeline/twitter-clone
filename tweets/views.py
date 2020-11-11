c041fae02ccb461bd1b5379302e79e3210537f17
alicia.maydeline <amangto@uw.washington.edu>
2020-11-10 22:05:06 -0800

    adding delete tweet

--- tweets/views.py
+++ tweets/views.py
@@ -41,3 +41,16 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
   obj = qs.first()
   serializer = TweetSerializer(obj)
   return Response(serializer.data, status=200)
+
+@api_view(['DELETE', 'POST'])
+@permission_classes([IsAuthenticated]) 
+def tweet_delete_view(request, tweet_id, *args, **kwargs):
+  qs = Tweet.objects.filter(id=tweet_id)
+  if not qs.exists():
+    return Response({}, status=404)   # not found
+  qs = qs.filter(user=request.user)
+  if not qs.exists():
+    return Response({"message": "You cannot delete this tweet"}, status=401)
+  obj = qs.first()
+  obj.delete()
+  return Response({"message": "Tweet deleted"}, status=200)
